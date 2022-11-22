d50d45b1b5dd27d5a71703ed383b4471753b1bcc
yonnyg23 <113356459+yonnyg23@users.noreply.github.com>
2022-11-22 14:41:03 -0500

    Final Version
    Final Version

--- diceroller/diceroller/app/controllers/HomeController.java
+++ diceroller/diceroller/app/controllers/HomeController.java
@@ -1,21 +1,35 @@
 package controllers;
 
 import play.mvc.*;
+import java.util.Random;
+import play.libs.Json;
+import java.util.ArrayList;
 
 /**
  * This controller contains an action to handle HTTP requests
  * to the application's home page.
  */
 public class HomeController extends Controller {
-
-    /**
-     * An action that renders an HTML page with a welcome message.
-     * The configuration in the <code>routes</code> file means that
-     * this method will be called when the application receives a
-     * <code>GET</code> request with a path of <code>/</code>.
-     */
+	ArrayList<Integer> list = new ArrayList<>();
+/**http://localhost:9000/roll*/	
     public Result index() {
         return ok(views.html.index.render());
     }
+/**http://localhost:9000/roll*/	
+	 public Result roll() {
+        Random ran = new Random();
+		int x = 1 + ran.nextInt(6);
+		return ok(Json.toJson(x));	
+    }
+/**http://localhost:9000/roll/6*/	
+	public Result rollMany(String n){
+		list.clear();
+		for (int i = 0; i < Integer.parseInt(n); i++) {
+			Random ran = new Random();
+			int x = 1 + ran.nextInt(6);
+			list.add(x);
+		}
+		return ok(Json.toJson(list));
+	}
 
 }
